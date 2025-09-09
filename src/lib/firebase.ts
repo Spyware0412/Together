4ccfd3c1b85cf33c701ab00ce1dce761bf0c6475
Rishu Gupta <rishuguptark@gmail.com>
2025-09-09 16:02:16 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBkh4nP34JkMnCN1NjA4Ju6p40uanVC

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBkh4nP34JkMnCN1NjA4Ju6p40uanVCYA8",
+  authDomain: "cinesync-b3jxg.firebaseapp.com",
+  databaseURL: "https://cinesync-b3jxg-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cinesync-b3jxg",
+  storageBucket: "cinesync-b3jxg.firebasestorage.app",
+  messagingSenderId: "40884970758",
+  appId: "1:40884970758:web:413a1062cff0d06eab2a87"
 };
 
 // Initialize Firebase
