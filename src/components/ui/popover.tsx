414af12540241ca1813bc124de0015aab94f0aee
Rishu Gupta <rishuguptark@gmail.com>
2025-09-13 23:24:19 +0000

    When I'm in fullscreen, the info and settting button font work; which ma

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -9,6 +9,8 @@ const Popover = PopoverPrimitive.Root
 
 const PopoverTrigger = PopoverPrimitive.Trigger
 
+const PopoverAnchor = PopoverPrimitive.Anchor
+
 const PopoverPortal = PopoverPrimitive.Portal
 
 const PopoverContent = React.forwardRef<
@@ -30,4 +32,4 @@ const PopoverContent = React.forwardRef<
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
-export { Popover, PopoverTrigger, PopoverContent, PopoverPortal }
+export { Popover, PopoverTrigger, PopoverContent, PopoverPortal, PopoverAnchor }
